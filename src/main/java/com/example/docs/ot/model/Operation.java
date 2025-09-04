baa7dad6420eec8f886a0a727b76259e6a6ca4a0
Aravind <aravind19112003@gmail.com>
2025-09-05 00:11:53 +0530

    Add operations transform

--- src/main/java/com/example/docs/ot/model/Operation.java
+++ src/main/java/com/example/docs/ot/model/Operation.java
@@ -1,9 +1,44 @@
 package com.example.docs.ot.model;
 
 public class Operation {
+
     public enum Type { INSERT, DELETE }
     Type type;
     int position;
     String character;
     int version;
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+
 }
---
+++ src/main/java/com/example/docs/ot/utils/OperationsTransformer.java
@@ -0,0 +1,39 @@
+package com.example.docs.ot.utils;
+
+import com.example.docs.ot.model.Operation;
+
+public class OperationsTransformer {
+    public static String apply(String doc, Operation op){
+        switch (op.getType()) {
+            case INSERT -> {
+                return doc.substring(0, op.getPosition()) + op.getCharacter() + doc.substring(op.getPosition());
+            }
+            case DELETE -> {
+                return doc.substring(0, op.getPosition()) + doc.substring(op.getPosition() + 1);
+            }
+            default -> throw new IllegalArgumentException("Unknown operation type!");
+        }
+    }
+
+    public static Operation transform(Operation op1, Operation op2){
+        if(op1.getType() == Operation.Type.INSERT && op2.getType() == Operation.Type.INSERT){
+            if(op1.getPosition() <= op2.getPosition()){
+                op2.setPosition(op2.getPosition() + 1);
+            }
+        } else if (op1.getType() == Operation.Type.DELETE && op2.getType() == Operation.Type.INSERT){
+            if(op1.getPosition() < op2.getPosition()){
+                op2.setPosition(op2.getPosition() - 1);
+            }
+        } else if (op1.getType() == Operation.Type.INSERT && op2.getType() == Operation.Type.DELETE){
+            if(op1.getPosition() <= op2.getPosition()){
+                op2.setPosition(op2.getPosition() + 1);
+            }
+        } else if (op1.getType() == Operation.Type.DELETE && op2.getType() == Operation.Type.DELETE){
+            if(op1.getPosition() < op2.getPosition()){
+                op2.setPosition(op2.getPosition() - 1);
+            }
+        }
+
+        return op2;
+    }
+}
